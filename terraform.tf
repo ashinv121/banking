185747d878133dd771f5cfac0f27cb65bcdee06a
Ashin v <37022429+ashinv121@users.noreply.github.com>
2023-08-28 21:43:28 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -64,6 +64,12 @@ resource "aws_security_group" "test_server_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
